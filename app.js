105c38526a633f35810d71d9df9d1d6c2d1f3d2e
Deyems <adeyemisola11@gmail.com>
2024-03-25 00:50:15 +0100

    the readline interface to handle commands

---
+++ app.js
@@ -0,0 +1,14 @@
+const {processCommand} = require("./src/modules/directory-api/commandLineHandler");
+const process = require("process");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    processCommand(input.trim());
+});
+
+console.log(`Enter commands (e.g., CREATE fruits/apples, MOVE fruits/apples foods, DELETE fruits/apples, LIST). Press Ctrl+C to exit.`);
