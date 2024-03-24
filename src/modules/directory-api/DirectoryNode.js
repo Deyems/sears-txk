a19287d797962db275347d57ccc998c28f6d4d52
Deyems <adeyemisola11@gmail.com>
2024-03-25 00:49:57 +0100

    directoryNode to define the interface for the tree with children and name constructor

---
+++ src/modules/directory-api/DirectoryNode.js
@@ -0,0 +1,33 @@
+class DirectoryNode {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(child) {
+        this.children.push(child);
+    }
+
+    removeChild(childName) {
+        this.children = this.children.filter(child => child.name !== childName);
+    }
+
+    getChild(childName) {
+        const found = this.children.find(child => child.name === childName);
+        return found;
+    }
+
+    /**
+     * 
+     * @param {*} prefix 
+     * @description recursive call
+     */
+    listDirectories(prefix = "") {
+        console.log(prefix + this.name);
+        this.children.forEach(child => {
+            child.listDirectories(prefix + "  ");
+        });
+    }
+}
+
+module.exports = DirectoryNode;
